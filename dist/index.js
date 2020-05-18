98327902c52f4c32703405861cc109fdff82d990
unknown <localadmin@IDC20Intern146.fareast.corp.microsoft.com>
2020-05-18 20:47:57 +0530

    removed @ v1

--- dist/index.js
+++ dist/index.js
@@ -94,7 +94,7 @@ const github = __webpack_require__(136);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello there ${nameToGreet}!`);
+  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
