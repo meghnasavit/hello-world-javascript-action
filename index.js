8731e1c4c558f0f5f32c766026e6373f94ffbb3f
Meghna Savit <meghnasavit@DESKTOP-P82ELB7.redmond.corp.microsoft.com>
2021-12-16 16:00:38 +0530

    dfdsf

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   var information = fs.readFileSync('dates.json');
   information = JSON.parse(information);
-  const dates = information.dates;
+  const dates = JSON.parse(information.dates);
   console.log(`dates: ${dates}`);
   console.log(`information: ${information}`);
 } catch (error) {
