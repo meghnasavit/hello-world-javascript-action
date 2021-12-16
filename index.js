f54fbc00dcd8503e3ccbb98654e1b44207035877
Meghna Savit <meghnasavit@DESKTOP-P82ELB7.redmond.corp.microsoft.com>
2021-12-16 15:40:14 +0530

    dfdsf

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  const date = new Date();
+  console.log(date);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
