fedace7363e18ab3019e45abee72072886903d8f
Meghna Savit <meghnasavit@DESKTOP-P82ELB7.redmond.corp.microsoft.com>
2021-12-16 12:35:09 +0530

    dfdsf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -8,7 +9,9 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const information = fs.readFileSync('package.json');
+  console.log(`information: ${information}`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
