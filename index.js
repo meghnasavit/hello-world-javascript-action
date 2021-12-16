6f95f85ae595231ef5b03732427e19f964ff269f
Meghna Savit <meghnasavit@DESKTOP-P82ELB7.redmond.corp.microsoft.com>
2021-12-16 15:50:24 +0530

    dfdsf

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ try {
   var dd = String(today.getDate()).padStart(2, '0');
   var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
   var yyyy = today.getFullYear();
-  today = mm + '/' + dd + '/' + yyyy;
+  today = dd + '/' + mm + '/' + yyyy;
   console.log(today);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const information = fs.readFileSync('package.json');
+  const information = fs.readFileSync('dates.json');
   console.log(`information: ${information}`);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payloaddgfdhfhg: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
